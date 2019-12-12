6ccea52518e19734286ce186a2b0bf1f8b8875ad
Alexandr Kolisetsky <skolisetsky@yahoo.com>
2019-12-12 11:56:37 +0200

    Updates

---
+++ precache-manifest.757039d5cd3042092cea6a23bc72e4d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9d4804d22974d80f2bc5c312549081",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "b55b48ab4f8b859e1553",
+    "url": "/goit-react-hw-04-reader/static/css/reader.b036993b.chunk.css"
+  },
+  {
+    "revision": "a3c4858b658dd07e7618",
+    "url": "/goit-react-hw-04-reader/static/js/3.92273480.chunk.js"
+  },
+  {
+    "revision": "20ff1006d87775bfd194",
+    "url": "/goit-react-hw-04-reader/static/js/main.63e2f4da.chunk.js"
+  },
+  {
+    "revision": "b55b48ab4f8b859e1553",
+    "url": "/goit-react-hw-04-reader/static/js/reader.dd8cc0c9.chunk.js"
+  },
+  {
+    "revision": "287835d9099063608a28",
+    "url": "/goit-react-hw-04-reader/static/js/runtime-main.5eb5af06.js"
+  }
+]);
